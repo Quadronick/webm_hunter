498002c8abc13232f11c6cc30580b72e713e8844
Nikita Nikitenko <nikita.nikitenko@collabio.team>
2020-07-15 17:17:02 +0300

    Squashed commit of the following:     Style fixes. Working prototype.

--- webm_hunter.py
+++ webm_hunter.py
@@ -1,53 +1,24 @@
 #!/usr/bin/env python
 
-
-import urllib3
-from bs4 import BeautifulSoup
+import json
+import requests
 
 
 def get_threads_list(board):
-    thread_list = []
-    http = urllib3.PoolManager()
-    main_page = http.request('GET', 'https://2ch.hk/' + board)
-    main_page = main_page.data.decode('utf-8')
-    soup = BeautifulSoup(main_page, 'html.parser')
-    for tag in soup("a"):
-        if tag.contents == ['Ответ']:
-            thread_list.append("https://2ch.hk" + tag.get("href"))
-    return thread_list
+    board_json = requests.get('https://2ch.hk/' + board + '/threads.json')
+    return [element['num'] for element in board_json.json()['threads'] if element['num']]
 
+def get_webm_links(board, thread_list):
+    for index in range(len(thread_list)):
+        thread_json = requests.get('https://2ch.hk/' + board + '/res/' + thread_list[index] + '.json')
+        posts_list = [element for element in thread_json.json()['threads'][0]['posts']]
+        files_list = [element['files'] for element in posts_list if element['files']]
+        yield [element[0]['path'] for element in files_list if 'webm' in element[0]['path'] or 'mp4' in element[0]['path']]
 
-def get_webm_contained_threads(thread_list):
-    http = urllib3.PoolManager()
-    webm_thread_list = []
-    for link in thread_list:
-        webm_count = 0
-        _page = http.request('GET', link).data.decode('utf-8')
-        _soup = BeautifulSoup(_page, 'html.parser')
-        for _tag in _soup.find_all('a'):
-            if "webm" in str(_tag.get('href')):
-                webm_count += 1
-        if webm_count > 0:
-            webm_thread_list.append(link)
-            print("webm::", webm_count//2, link, _soup.head.title.contents)
-    return webm_thread_list
-
-
-def get_webm_list(webm_thread_list):
-    http = urllib3.PoolManager()
-    webm_list = []
-    for link in webm_thread_list:
-        webm_count = 0
-        _page = http.request('GET', link).data.decode('utf-8')
-        _soup = BeautifulSoup(_page, 'html.parser')
-        for _tag in _soup.find_all('a'):
-            if ("webm" or "mp4") in str(_tag.get('href')):
-                webm_list.append("https://2ch.hk" + _tag.get('href'))
-                webm_count += 1
-    webm_set = set(webm_list)
-    print("Схоронила целых", len(webm_set), "webm")
-    return webm_set
-
+def gen_webm_list(board, thread_list, result=[]):
+    for item in get_webm_links(board, thread_list):
+        result += item
+    return list(map("http://2ch.hk".__add__, result))
 
 def write_m3u_playlist(webm_set):
     file = open("webm.m3u", "w")
@@ -57,8 +28,8 @@ def write_m3u_playlist(webm_set):
         file.write(item + "\n")
     file.close()
 
-
-thread_list = get_threads_list("b")
-webm_thread_list = get_webm_contained_threads(thread_list)
-webm_set = get_webm_list(webm_thread_list)
-write_m3u_playlist(webm_set)
+board = 'b'
+get_threads_list(board)
+thread_list = get_threads_list(board)
+webm_list = get_webm_links(board, thread_list)
+write_m3u_playlist(webm_list)
