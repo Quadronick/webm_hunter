32d9adfd0a2d89b9cfaf3f80a81f09a3c4de1879
Nikita Nikitenko <nikita.nikitenko@collabio.team>
2021-04-08 00:48:12 +0300

    WH-001: Add verbose flag (#1)
    Merge wh-001-add-verose-flag into the master.

--- webm_hunter.py
+++ webm_hunter.py
@@ -2,10 +2,20 @@
 
 import argparse
 import requests
+import sys
 
 parser = argparse.ArgumentParser(description='Generate M3U playlist full of strange videos')
-parser.add_argument('--board', action="store", dest="board", type=str, help="Codename of the board; String;", required=True)
+parser.add_argument('--board', action="store", type=str, help="Codename of the board; e.g a, b, rf, wh...", required=True)
+parser.add_argument('--verbose', action="store_true", help="Increase output verbosity")
 args = parser.parse_args()
+
+
+def progress_bar(index, max_index, thread_name, files_found):
+    sys.stdout.write('\r')
+    status = round(index / max_index * 50)
+    info = (thread_name[:35] + '..') if len(thread_name) > 35 else thread_name
+    sys.stdout.write('[' + status * '*' + (50 - status) *' ' + ']' + ' ' + str(len(files_found)) + ' ' + info)
+    sys.stdout.write('\r')
 
 
 def get_threads_list(board):
@@ -17,6 +27,8 @@ def get_webm_links(board, thread_list):
         thread_json = requests.get('https://2ch.hk/' + board + '/res/' + thread_list[index] + '.json')
         posts_list = [element for element in thread_json.json()['threads'][0]['posts']]
         files_list = [element['files'] for element in posts_list if element['files']]
+        if args.verbose:
+            progress_bar(index, len(thread_list), thread_json.json()['title'], files_list)
         yield [element[0]['path'] for element in files_list if 'webm' in element[0]['path'] or 'mp4' in element[0]['path']]
 
 def gen_webm_list(board, thread_list):
@@ -33,9 +45,10 @@ def write_m3u_playlist(webm_set):
         file.write(item + "\n")
     file.close()
 
-get_threads_list(args.board)
 thread_list = get_threads_list(args.board)
+if args.verbose:
+    print("Got", len(thread_list), "threads!" )
 webm_list = gen_webm_list(args.board, thread_list)
 write_m3u_playlist(webm_list)
-
+print('\n')
 print('All done!', len(webm_list), '<:::> videos found!')
